09f94ee0555e2d710bb32223a1c262973859e366
Ying-An <yingan.wang@berkeley.edu>
2023-06-06 20:19:57 -0700

    convert image to url, resize and then to blob
    Co-authored-by: Jessica <jessicavo@users.noreply.github.com>
    Co-authored-by: Scott Kim <skottcim@users.noreply.github.com>

--- scripts_og/writeEmojiToClipboard.ts
+++ scripts_og/writeEmojiToClipboard.ts
@@ -12,32 +12,37 @@ const resizeImage = (imgElement: HTMLImageElement, targetWidth: number = 64): HT
 }
 
 
-// when clicking merged emoji, parse through event target to find image file
+// when clicking merged emoji, parse through event target to find image file (blob)
 // then on click convert png to dataURL
-const writetoClipboard = async (image) => {
+const writetoClipboard = async (image: URL) => {
     const emoji = await fetch(image).then(r => r.blob());
-    const dataUrl : Promise = await new Promise(resolve => {
+    const dataUrl: string = await new Promise(resolve => {
         let reader = new FileReader();
-        reader.onload = () => resolve(reader.result);
-        reader.readAsDataURL(emoji);
+        reader.onload = () => resolve(reader.result as string);
+        reader.readAsDataURL(emoji); //this converts the blob to data URI
       });
     
     const imageEl : HTMLImageElement = document.createElement('img');
     imageEl.src = dataUrl;
     
+    //this will return an canvas element
     const canvas: HTMLCanvasElement = resizeImage(imageEl);
-    //write blob to clipboard
+
+    canvas.toBlob(async (blob) => {
+      //checking for null or else typescript yells at us
+      if (blob) await navigator.clipboard.write([new ClipboardItem({'image/png': blob})])
+    });
 }
 
 document.addEventListener('mergedEmojiClicked', (e) => {
   // writetoClipboard(e.target.image)
-})
+}) 
+
 
 
 //resize image to a canvas using drawImage
 //convert canvas to blob
 //copy blob to clipboard
-// canvas.toBlob(blob => navigator.clipboard.write([new ClipboardItem({'image/png': blob})]))
 
 //convert target png to uri
 //create new img element
